1e34eece4770cc003d46fe76b843fdcc3021bff3
ajsheehan1 <alexander.j.sheehan@gmail.com>
2020-06-23 14:53:36 -0400

    Add project files.

---
+++ 00_VariablesAndValueTypes/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_VariablesAndValueTypes
+{
+    enum PastryType{Cake, Cupcake, Croissant, Donut, Cookie, Scone, Danish }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is how you take notes
+
+            // Bools
+            bool isDeclared;
+            isDeclared = true;
+
+            bool isDeclaredAndInitialized = false;
+            isDeclaredAndInitialized = true;
+
+            // Characters - can only initialize single digits
+            char character = 'a';
+            char symbol = '&';
+            char number = '3';
+            char space = ' ';
+            char specialCharacters = '\n';
+
+            // Wole numbers
+            // All numbers listed below have a certain number of digits
+            // No quotes because you are assigning a numerical value 
+            byte byteExample = 255;
+            // byte max is 255 ^^ 
+            sbyte sByteExample = -128;
+            // represents an 8-bit signed integer
+
+            // short and Int16 are the same
+            short shortExample = 32767;
+            Int16 anotherShortExample = 32767;
+
+            //Int is most commonly used
+            int intMin = -2147483648; // smallest an Int can be
+            Int32 intMax = 2147483647; // highest an Int can be
+
+            // long is longer than int - same as INT64
+            long longExample = 9223372036854775807;
+            Int64 longMin = -9223372036854775808;
+
+            int a = 7;
+            int b = -7000;
+
+            byte age = 104;
+
+
+            // Decimals - 
+            // Float - MUST have the f at the end, 
+            // double- doesn't necessarily need the d
+            float floatExample = 1.045231f;
+            double doubleExample = 1.789053278907036d;
+            decimal decimalExample = 1.2578907289045789789789789787897m;
+            Console.WriteLine(1.2578907289045789789789789787897f); // prints 1.257891
+            Console.WriteLine(1.2578907289045789789789789787897d); // prints 1.25789072890458
+            Console.WriteLine(1.2578907289045789789789789787897m); // prints 1.2578907289045789789789789788
+
+
+            Console.ReadKey();
+
+            // Enums
+            PastryType myPastry = PastryType.Croissant;
+            PastryType anotherOne = PastryType.Donut;
+
+            //Structs 
+            Int32 num = 42;
+            DateTime today = DateTime.Today;
+            DateTime birthday = new DateTime(1805,11,24);
+            DateTime test = new DateTime();
+
+        }
+    }
+}
---
+++ 01_ReferenceTypes/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Strings 
+            string thisIsDeclaration;
+            string declared;
+            declared = "This is initialized.";
+
+            string declarationAndInitialization = "This is both declaring and initializing.";
+
+            //
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+
+
+            //Concatenation
+            string concatenatedFullName = firstName + " " + lastName;
+            //Composite 
+            string compositeFullName = string.Format("{0} {1}", firstName, lastName);
+            // interpolation
+            string interpolatedFullName = $"{firstName} {lastName}";
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(concatenatedFullName);
+            Console.WriteLine(compositeFullName);
+            Console.WriteLine(interpolatedFullName);
+
+
+            // -- Collections
+            // Array
+            string stringExample = "Hello World!";
+            string[] stringArray = { "Hello", "World!", "Why", "is it", "always", stringExample, "?" };
+
+            string thirdItem = stringArray[2];
+            Console.WriteLine(thirdItem);
+            stringArray[0] = "Hey there";
+            Console.WriteLine(stringArray[0]);
+
+            // -- Lists
+            List<string> listofStrings = new List<string>();
+            List<int> listOfIntegers = new List<int>();
+            listofStrings.Add("First string of our list");
+            listOfIntegers.Add(4242);
+            listOfIntegers.Add(12345);
+
+            Console.WriteLine(listOfIntegers[0]);
+
+            // -- Queue
+            Queue<string> firstInFirstOut = new Queue<string>();
+            firstInFirstOut.Enqueue("I'm first!");
+            firstInFirstOut.Enqueue("I'm next");
+            string firstItem = firstInFirstOut.Dequeue();
+            Console.WriteLine(firstItem);
+
+    
+            //Dictionairies
+            Dictionary<int, string> keyAndValue = new Dictionary<int, string>();
+            keyAndValue.Add(7, "Agent");
+            keyAndValue.Add(8, "Agent2");
+            string valueSeven = keyAndValue[7];
+            Console.WriteLine(valueSeven);
+
+            // Other collection examples
+            SortedList<int, string> sortedKeyAndValues = new SortedList<int, string>();
+            HashSet<int> uniqueList = new HashSet<int>();
+            Stack<string> lastInFirstOut = new Stack<string>();
+
+            //Classes 
+            
+            Random rng = new Random();
+            int randomNumer = rng.Next();
+            Console.WriteLine(randomNumer);
+
+            Console.ReadKey();
+        }
+    }
+}
